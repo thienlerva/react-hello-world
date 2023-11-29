0c01f619c1ed561e3883ed49dc9cbb7e1bb47454
Thienlerva <thienle112014@gmail.com>
2023-11-29 11:07:20 -0500

    added command list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function() { //load first advice
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {} to use javascript */}
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>get advice</button>
+      <Message count={count}/>
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>You have read <strong>{props.count}</strong> pieces of advice</p>
   );
 }
 
